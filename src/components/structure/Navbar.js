6a8175503db810481cdeab71bffe0e245332ae41
El SantyM <52477105+SantyM19@users.noreply.github.com>
2021-04-30 11:22:51 -0500

    Cambios de ortografía en las notificaciones

--- src/components/structure/Navbar.js
+++ src/components/structure/Navbar.js
@@ -17,44 +17,30 @@ function Navbar() {
     data: [
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3a',
-            name: 'Notification Proyect',
+            name: 'Proyecto de Notificaciones',
             typeN: 'OKR',
             detail: '30',
             viewed: 'false'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect',
+            name: 'Proyecto de Notificaciones',
             typeN: 'OKR',
             detail: '70',
             viewed: 'true'
         },
         {
-            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect',
-            typeN: 'OKR',
-            detail: '100 Or Finished',
-            viewed: 'true'
-        },
-        {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3a',
-            name: 'Notification Proyect',
+            name: 'Interacción Campana',
             typeN: 'KR',
             detail: '30',
             viewed: 'true'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect',
+            name: 'Interacción Campana',
             typeN: 'KR',
             detail: '70',
-            viewed: 'true'
-        },
-        {
-            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect ',
-            typeN: 'KR',
-            detail: '100',
             viewed: 'true'
         },
     ],
